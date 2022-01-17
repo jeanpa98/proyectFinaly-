ff049885e86678379500af97f3419b3824bcb820
Jean Panamito <jeanpanamito@gmail.com>
2022-01-16 22:00:15 -0500

    cambios

---
+++ proyectoFinal/src/proyectofinal/alumno.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectofinal;
+
+/**
+ *
+ * @author JEanpa
+ */
+public class alumno {
+    // nombre - curso - materias-grado
+     private String nombre;
+     private char curso;
+     private char materia;
+     private int grado;
+     
+     public alumno(){
+     nombre = "";
+     curso = ' ';
+     materia = ' ';
+     grado = 0;
+     
+     }
+     
+     
+}
+
